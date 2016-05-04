6de0d80af1787b03003b463b993963adea6aded8
aslanahmet <ahmet@kodeveloper.co>
2016-05-04 10:17:09 +0300

    test

--- webServices/Controller/Requests/Web/addpartnerUsers.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../../Partner/Web/PartnerController.php';
-$pName=$_POST['pName'];
-$userName=$_POST['userName'];
-$password=$_POST['password'];
-$userType=$_POST['userType'];
-$addPartnerUser=new PartnerController();
-$addPartnerUser->addPartnerUsers($pName,$userName,$password,$userType);
-?>
