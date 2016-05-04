7baf3c7779f0a7c086087bf3728c70bca04896b7
Oğuzhan Demiröz <oguzhan@kodeveloper.co>
2016-05-04 17:46:17 +0300

    config

--- webServices/Config/Connect/connect.php
+++ webServices/Config/Connect/connect.php
@@ -6,7 +6,7 @@ class ConnectPayponseDb{
     function __construct($db)
     {
         $dbName=trim(htmlspecialchars($db));
-        $db = 'mysql:host=localhost;dbname='.$dbName.'';
+        $db = 'mysql:host=160.153.162.194;dbname='.$dbName.'';
         $user = 'pp_write';
         $pass = 'tJ9MY;3X+#6P#u+64LoTtKV';
         $this->temp = new PDO($db, $user, $pass);
